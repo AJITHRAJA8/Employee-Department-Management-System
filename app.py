ac3bae30413289152eedd4db2bf91aa18f934be1
AJITHRAJA8 <ajithrajar79@gmail.com>
2026-06-10 14:23:18 +0530

    create and complete the update employee page

--- app.py
+++ app.py
@@ -26,21 +26,43 @@ def home():
     return render_template("home.html", datas=result)
 
 #Update EMployee Tabel
-@app.route('/update/<int:id>', methods=['GET','POST'])
+@app.route('/update/<int:id>',methods=['GET','POST'])
 def update(id):
+    res=con.cursor(dictionary=True)
     if request.method=='POST':
         name=request.form['name']
-        dept_id=request.form['dept_id']
-        print(request.form)
         salary=request.form['salary']
         city=request.form['city']
-        res=con.cursor(dictionary=True)
+        dept_id=request.form['dept_id']
+
         sql='update employee set name=%s,salary=%s,city=%s,dept_id=%s where id=%s'
         value=(name,salary,city,dept_id,id)
         res.execute(sql,value)
         con.commit()
         return redirect(url_for('home'))
-    return render_template('update.html')
+    # Employee Detials
+    sql='select * from employee where id=%s'
+    value=(id,)
+    res.execute(sql,value)
+    employee=res.fetchone()
+    #All Departments
+    res.execute('select * from department')
+    departments=res.fetchall()
+    return render_template(
+            "update.html",
+            employee=employee,
+            departments=departments
+        )
 
+#search Employee
+@app.route('/search',methods=['GET','POST'])
+def search():
+    search=request.form['search']
+    res=con.cursor(dictionary=True)
+    sql='select * from employee where name like %s'
+    value=('%' +search+ '%',)
+    res.execute(sql,value)
+    result=res.fetchall()
+    return render_template("home.html",datas=result)
 if(__name__)=="__main__":
     app.run(debug=True,port=8000)
