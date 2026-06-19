a29a4def026b21625fe9b3c82b028babd6d0e5bf
AJITHRAJA8 <ajithrajar79@gmail.com>
2026-06-19 22:22:59 +0530

    hash the password

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ app=Flask(__name__)
 con=mysql.connector.connect(
     host="localhost",
     user="root",
-    password="Your_password",
+    password="",
     database="mangement"
 )
 if con.is_connected:
